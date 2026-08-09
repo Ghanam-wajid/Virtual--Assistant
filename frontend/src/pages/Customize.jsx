757b4dd23df2ff301d81dbb38d92f78eb4fd0c5a
Ghanam wajid <ghanam8755@gmail.com>
2026-08-09 08:21:12 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -9,7 +9,7 @@ import image6 from "../assets/image6.jpeg"
 import image7 from "../assets/image7.jpeg"
 import { RiImageAddFill } from "react-icons/ri"
 import { useContext } from 'react'
-import { userDataContext } from '../context/userContext'
+import { userDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 import { MdKeyboardBackspace } from "react-icons/md";
 
